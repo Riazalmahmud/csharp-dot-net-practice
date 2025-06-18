876b00f77d2f18226751fa094943fd36bc400432
Riazalmahmud <riazalmahmud041@gmail.com>
2025-06-18 21:06:32 +0600

    program with reflection

--- Program.cs
+++ Program.cs
@@ -1,33 +1,42 @@
-﻿#define DEBUG
-using System;
-using System.Diagnostics;
+﻿using System;
+using System.Dynamic;
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
 
+class HelpAttribute : System.Attribute
+{
 
+  public readonly string url;
+
+  private string topic;
+
+  public string Topic
+  {
+    get { return topic; }
+    set { topic = value; }
+  }
+  public HelpAttribute(string url)
+  {
+    this.url = url;
+
+  }
+
+}
+// Apply the attribute to a class
+[Help("Information on the class MyClass", Topic = "Class Info")]
 class MyClass
 {
-  [Conditional("DEBUG")]
-  public static void Massage(string msg)
-  {
-    Console.WriteLine(msg);
-  }
 }
 
-
-class Test
+namespace ReflectionTest
 {
-  static void Function1()
-  {
-    MyClass.Massage("In function 1");
-  }
-  static void Function2()
-  {
-    MyClass.Massage("In function 2");
-  }
 
 
-  public static void Main(string[] args)
+  class ReflectionClass
   {
-    MyClass.Massage("In Main function.");
-    Function1();
+    static void Main(string[] args)
+    {
+System.Reflection.MethodInfo MemberInfo info = typeof(MyClass);
+object[] at 
+    }
   }
 }
