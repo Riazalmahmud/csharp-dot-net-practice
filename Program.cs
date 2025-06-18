13d6a71d0f3bcb4907ed610fc98e0e8b24dec44a
Riazalmahmud <riazalmahmud041@gmail.com>
2025-06-18 13:04:21 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿#define DEBUG
+using System;
+using System.Diagnostics;
+
+
+class MyClass
+{
+  [Conditional("DEBUG")]
+  public static void Massage(string msg)
+  {
+    Console.WriteLine(msg);
+  }
+}
+
+
+class Test
+{
+  static void Function1()
+  {
+    MyClass.Massage("In function 1");
+  }
+  static void Function2()
+  {
+    MyClass.Massage("In function 2");
+  }
+
+
+  public static void Main(string[] args)
+  {
+    MyClass.Massage("In Main function.");
+    Function1();
+  }
+}
